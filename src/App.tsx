ab7d7442fb31c876a29ef9e62a3eab68c31ddf99
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-07 00:52:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-red-500 hover:text-blue-500 cursor:pointer'>hello</h2>
+    </>
+  )
+}
+
+export default App
