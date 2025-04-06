b336c1b620b978fdad6b7fc5eb3e2af2160b2995
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-07 01:31:16 +0530

    Update Vite configuration to include babel-plugin-react-compiler in React plugin settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import TanStackRouterVite from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react(),tailwindcss()],
+  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react({
+    babel: {
+      plugins: [["babel-plugin-react-compiler"]]
+    }
+  }),tailwindcss()],
   server: {
     port: 3200
   }
