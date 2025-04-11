fcdabaeb32fc0a0e0da5a3af3b8f697ef013c4d9
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-11 22:35:08 +0530

    feat: implement error handling service for improved error notifications
    This commit introduces a new errorService module that provides a function to handle errors, specifically for Axios requests. It captures error messages and displays notifications, enhancing user feedback during error scenarios.

---
+++ src/service/errorService.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { errorNotification } from "../components/notification/notification";
+
+
+export function handleError(err: unknown, notificationTitle?: string) {
+    let errMsg: string;
+    if (axios.isAxiosError(err) && err.response?.data?.errorMessage) {
+        errMsg = err.response?.data?.errorMessage
+        console.log(errMsg);
+    } else {
+        errMsg = "An unexpected error occurred"
+        console.log(errMsg, err);
+    }
+
+    if (notificationTitle && notificationTitle.length > 0) {
+        errorNotification(notificationTitle, errMsg);
+    }
+
+}
