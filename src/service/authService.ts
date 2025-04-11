b3d302cd49679f6058819d16e3678a165d4ec48e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-11 21:40:51 +0530

    feat: implement authentication service with user registration, login, OTP handling
    This commit introduces a new authService module that includes functions for user registration, login, sending OTPs, and verifying OTPs. It utilizes axios for HTTP requests to the authentication API, enhancing the application's authentication capabilities.

---
+++ src/service/authService.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { LoginType, RegisterUserType } from "../types/authType";
+
+const base_URL = "http://localhost:8080/auth"
+
+export const registerUser = async (user: RegisterUserType) => {
+    try {
+        const response = await axios.post(`${base_URL}/register`, user);
+        return response.data;
+    } catch (error: unknown) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const loginUser = async (login: LoginType) => {
+    try {
+        const response = await axios.post(`${base_URL}/login`, login);
+        return response.data;
+    } catch (error: unknown) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const sendOtp = async (email: string, check: string) => {
+    try {
+        const params = { email, check };
+        const res = await axios.post(`${base_URL}/sendOtp`, null, { params });
+        return res.data;
+    } catch (error: unknown) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const verifyOtp = async (email: string, otp: string) => {
+    try {
+        const res = await axios.get(`${base_URL}/verifyOtp/${email}/${otp}`);
+        return res.data;
+    } catch (error: unknown) {
+        console.log(error);
+        throw error;
+    }
+}
+
