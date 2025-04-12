4fb5b1fb21f8fb9d12f4c17735bdd035725e4390
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-12 22:56:08 +0530

    feat(validation): update signup form validation rules for username length and account type
    This commit modifies the signupFormValidation schema to restrict the username length to a maximum of 60 characters and adds a custom error message for the account type selection. Additionally, it introduces a validation rule for accepting terms and conditions, enhancing the overall user registration process.

--- src/validators/signupFormValidation.ts
+++ src/validators/signupFormValidation.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const signupFormValidation = z.object({
-    username: z.string().min(3, "Name must contain 3 to 100 characters")
-        .max(100, "Name must not exceed 100 characters"),
+    username: z.string().min(3, "Name must contain 3 to 60 characters")
+        .max(60, "Name must not exceed 60 characters"),
     email : z.string().email("Please enter a valid email"),
     password : z.string().min(8, "Password must contain 8 to 15 characters")
         .max(15, "Password must not exceed 16 characters")
@@ -10,7 +10,12 @@ export const signupFormValidation = z.object({
         .regex(/\d/, "Password must contain at least one digit")
         .regex(/[@$!%*?&#]/, "Password must contain at least one special character"),
     confirmPassword : z.string(),
-    accountType: z.enum(["APPLICANT", "EMPLOYER"]),
+    accountType: z.enum(["APPLICANT", "EMPLOYER"], {
+        message: "account type must be either applicant or employer"
+    }),
+    TermsAndConditions : z.boolean().refine((value) => value,{
+        message: "You must accept the terms & conditions"
+    })
 })
     .refine((data) => data.password === data.confirmPassword,{
         message: "Passwords do not match",
