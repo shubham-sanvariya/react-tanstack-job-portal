f54190ea89d2f9cd1b5c768974785e405928fdb8
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-17 22:09:36 +0530

    feat(api): add axios configuration with response interceptor for token refresh
    This commit introduces a new axios configuration file that sets up an API instance with a base URL and includes a response interceptor. The interceptor handles 401 errors by attempting to refresh the authentication token and redirects the user to the login page if the session has expired. This enhancement improves the authentication flow and user experience by managing session expiration seamlessly.

---
+++ src/apiConfig/axiosConfig.ts
@@ -0,0 +1,44 @@
+import axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from "axios";
+import {errorNotification} from "../components/notification/notification.tsx";
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL,
+    withCredentials: true
+})
+
+let refreshTokenPromise: Promise<AxiosResponse<string>> | null = null;
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error: AxiosError) => {
+        const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean };
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            if (!refreshTokenPromise) {
+                refreshTokenPromise = api.post(`/auth/refresh-token`)
+                    .finally(() => {
+                        refreshTokenPromise = null;
+                    })
+            }
+
+            try {
+                await refreshTokenPromise;
+                return api(originalRequest);
+            } catch (refreshError) {
+                errorNotification("Session Expired", "Please Login again.")
+                localStorage.removeItem("user");
+
+                setTimeout(() => {
+                    window.location.href = "/login";
+                }, 3000)
+                console.log(refreshError);
+
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+)
