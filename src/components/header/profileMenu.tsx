4e3dc93731dd63d0c2cd1e2fcf626fc2ce7cc8a9
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-24 02:41:33 +0530

    feat(components): add ProfileMenu component for user profile management
    This commit introduces the ProfileMenu component, which provides a user interface for accessing profile-related features such as viewing the profile, messages, resume, toggling dark mode, and logging out. The component utilizes the useUser hook to display user information and enhances the overall user experience by centralizing profile management functionalities.

---
+++ src/components/header/profileMenu.tsx
@@ -0,0 +1,78 @@
+import {Menu, Avatar, Switch, rem} from '@mantine/core';
+import {
+    IconMessageCircle, IconUserCircle, IconFileText, IconMoon, IconSun, IconMoonStars, IconLogout2,
+} from '@tabler/icons-react';
+import {useState} from "react";
+import {Link} from "@tanstack/react-router";
+import useUser from "../../hooks/useUser.tsx";
+import {handleLogout} from "../../service/authService.ts";
+
+
+const ProfileMenu = () => {
+
+    const [checked, setChecked] = useState(false);
+    const [opened, setOpened] = useState(false);
+    const { profile } = useUser();
+
+
+    return (
+        <Menu opened={opened} onChange={setOpened} shadow="md" width={200}>
+            <Menu.Target>
+                <div className={'flex items-center gap-2 cursor-pointer'}>
+                    <div className='xs-mx:hidden'>
+                        {profile?.name}
+                    </div>
+                    <Avatar
+                        src={profile?.picture ? `data:image/jpeg;base64,${profile.picture}` : '/assets/avatar.png'}/>
+                </div>
+            </Menu.Target>
+
+            <Menu.Dropdown onChange={() => setOpened(true)}>
+                <Link to={'/profile'}>
+                    <Menu.Item leftSection={<IconUserCircle size={14}/>}>
+                        Profile
+                    </Menu.Item>
+                </Link>
+                <Menu.Item leftSection={<IconMessageCircle size={14}/>}>
+                    Messages
+                </Menu.Item>
+                <Menu.Item leftSection={<IconFileText size={14} style={{width: rem(14), height: rem(14)}}/>}>
+                    Resume
+                </Menu.Item>
+                <Menu.Item
+                    leftSection={<IconMoon size={14}/>}
+                    rightSection={
+                        <Switch
+                            checked={checked}
+                            onChange={(e) => setChecked(e.currentTarget.checked)}
+                            size={'md'} color={'dark4'}
+                            onLabel={
+                                <IconSun stroke={2.5}
+                                         style={{width: rem(16), height: rem(16)}}
+                                         color={'yellow'}
+                                />}
+                            offLabel={<IconMoonStars
+                                style={{width: rem(16), height: rem(16)}}
+                                stroke={2.5}
+                                color={'cyan'}
+                            />}
+                        />
+                    }
+                >
+                    Dark Mode
+                </Menu.Item>
+
+                <Menu.Divider/>
+
+                <Menu.Item
+                    onClick={handleLogout}
+                    color={'red'}
+                    leftSection={<IconLogout2 style={{width: rem(14), height: rem(14)}} size={14}/>}
+                >
+                    Logout
+                </Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
+    );
+}
+export default ProfileMenu
