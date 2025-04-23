6d26e7c6d888fa417d255624dfcbd9fce39cfff7
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-24 00:34:54 +0530

    feat(types): add NotificationType interface for notification management
    This commit introduces the NotificationType interface, defining the structure for notification objects within the application. It includes properties such as id, userId, message, action, route, status, and timeStamp, enhancing type safety and clarity for notification handling.

---
+++ src/types/notificationTypes.ts
@@ -0,0 +1,9 @@
+export interface NotificationType{
+    id : string;
+    userId : string;
+    message : string;
+    action : string;
+    route : string;
+    status : string;
+    timeStamp : string;
+}
