c0ccf9157ca23df0caf871c5e94c3b1574ce0932
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-24 02:41:12 +0530

    feat(hooks): enhance useUser hook to include user profile data retrieval
    This commit updates the useUser hook to fetch user profile data using the new getProfileById function. The hook now returns additional properties for the user profile, including loading and error states, improving the overall user data management and experience.

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,15 +1,24 @@
 import {useQuery} from "@tanstack/react-query";
 import {getUser} from "../service/userService.ts";
+import {getProfileById} from "../service/profileService.ts";
+import {ProfileType} from "../types/profileType.ts";
 
 const useUser = () => {
+
     const userState =  useQuery({
         queryKey: ["user"],
         queryFn: getUser,
-        staleTime: Infinity
-    })
+    });
 
     const { data : user, isPending, error } = userState;
 
-    return { user, isPending, error };
+    const profileState = useQuery<ProfileType>({
+        queryKey: ["profile", user?.profileId],
+        queryFn: async () => getProfileById(Number(user?.profileId))
+    })
+
+    const { data : profile, isPending : isUserProfilePending, error: userProfileError } = profileState;
+
+    return { user, isPending, error, profile, isUserProfilePending, userProfileError };
 }
 export default useUser
