6fa306840d926f7a914b932fdb77cfbb5fd13112
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-24 22:37:51 +0530

    feat(utils): add getBase64 utility function for file conversion
    This commit introduces a new utility function, getBase64, which converts a given file into a base64 string. The function handles null files and utilizes the FileReader API to read the file, enhancing the application's file handling capabilities.

--- src/components/utils/utility.ts
+++ src/components/utils/utility.ts
@@ -3,3 +3,19 @@ export const formatDate = ( dateString : string) => {
     const options = { year : 'numeric' as const, month : 'short' as const};
     return date.toLocaleString('en-US',options)
 }
+
+export const getBase64 = async (file: File | null): Promise<string> => {
+    if (file === null) return "";
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            if (typeof reader.result === 'string') {
+                resolve(reader.result); // Resolve with the base64 string
+            } else {
+                reject(new Error('Failed to read file as base64.'));
+            }
+        };
+        reader.onerror = (error) => reject(error); // Reject on error
+    });
+};
