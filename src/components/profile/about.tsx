a3e70d93bc399996b4105705367e2fc3b92de726
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-24 18:36:37 +0530

    refactor(components): update About component to use useProfile hook
    This commit refactors the About component to utilize the new useProfile hook for managing user profile data. It removes the previous useUser hook and associated mutation logic, streamlining the profile update process while maintaining functionality for editing and saving the About section.

--- src/components/profile/about.tsx
+++ src/components/profile/about.tsx
@@ -2,20 +2,17 @@ import {useState} from 'react'
 import {ActionIcon, Textarea} from "@mantine/core";
 import {IconCheck, IconPencil, IconX} from "@tabler/icons-react";
 import {useMediaQuery} from "@mantine/hooks";
-import useUser from "../../hooks/useUser.tsx";
 import {successNotification} from "../notification/notification.tsx";
-import {useMutation} from "@tanstack/react-query";
-import {updateProfile} from "../../service/profileService.ts";
 import {ProfileType} from "../../types/profileType.ts";
-import queryClient from "../../service/queryClient.ts";
-import {handleError} from "../../service/errorService.ts";
+import useProfile from "../../hooks/useProfile.tsx";
 
 const About = () => {
     const [edit, setEdit] = useState<boolean>(false);
     const matches = useMediaQuery('(min-width: 475px)');
-    const {profile: profileState, user} = useUser();
+    const {profile: profileState} = useProfile();
 
     const [about, setAbout] = useState(profileState?.about || "");
+    const { updateProfileMutate } = useProfile();
 
     const handleEdit = () => {
         if (!edit) {
@@ -26,36 +23,9 @@ const About = () => {
         }
     }
 
-    const {mutate: updateProfileMutate} = useMutation({
-        mutationFn: updateProfile,
-        onMutate: async (updatedProfile: ProfileType) => {
-            await queryClient.cancelQueries({
-                queryKey: ["userProfile", user?.profileId]
-            });
-
-            const previousProfile = queryClient.getQueryData<ProfileType>(["userProfile", user?.profileId]);
-
-            queryClient.setQueryData(["userProfile", user?.profileId], updatedProfile);
-
-            return {previousProfile}
-        },
-        onError: (error, _, context) => {
-            if (context?.previousProfile){
-                queryClient.setQueryData(['userProfile', user?.id], context.previousProfile);
-            }
-            handleError(error,"Failed to update About section");
-        },
-        onSettled: () => {
-            queryClient.invalidateQueries({
-                queryKey: ["userProfile", user?.profileId]
-            }).then();
-        }
-    })
-
     const handleSave = () => {
         setEdit(false);
         const updatedProfile = {...profileState, about};
-        // dispatch(updateProfileAsyncThunk(updatedProfile as ProfileType));
         updateProfileMutate(updatedProfile as ProfileType);
 
         successNotification("Success", "About Updated Successfully.")
