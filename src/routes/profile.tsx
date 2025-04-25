76608638acf87f025ee837c14d73d280a3d008d7
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-25 10:53:02 +0530

    feat(routes): update profile route to include Profile component and layout enhancements
    This commit modifies the profile route to render the Profile component within a styled layout. It adds a Divider for visual separation and adjusts the container's styling for improved user experience on the profile page.

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {Divider} from "@mantine/core";
+import Profile from "../components/profile/profile.tsx";
 
 export const Route = createFileRoute('/profile')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/profile"!</div>
+  return <div className="min-h-[90vh] bg-mine-shaft-950 font-[poppins] overflow-hidden">
+    <Divider mx={'md'} mb={'xl'}/>
+    <Profile/>
+  </div>
 }
